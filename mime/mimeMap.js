da9999d47cb7ccd0622a4b834fb84b7870a3e815
liulilili <35912362+liulilili@users.noreply.github.com>
2018-03-08 23:12:19 +0800

    Update mimeMap.js

--- mime/mimeMap.js
+++ mime/mimeMap.js
@@ -7,16 +7,16 @@ function sell() {
 
   while (time > 0) {
     var randList = new Array();
-    randList[time] = Math.random();
+    randList[time] = Math.floor(Math.random() * 10 + 1);
     time --;
   }//生成十个0-1随机数
   while(count<=10){
     if (randList[count] <= 0.6){
-      coins = 1.2 * coins;
+      coins = (1 + Math.random()) * coins;
     }else if (0.6< randList[count] <=0.8){
       coins = 1 * coins;
     }else{
-      coins = 0.8 * coins;
+      coins = (1 - Math.random()) * coins;
     }//市场价格对应的变化
 
 
